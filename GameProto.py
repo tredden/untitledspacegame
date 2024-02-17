e6612e80fcc656826c8703ea8df00b7e34346ac4
David <132954825+David8836@users.noreply.github.com>
2024-02-16 18:34:16 -0800

    Add the spaceship icon in the "Current ship" box

--- GameProto.py
+++ GameProto.py
@@ -7,38 +7,9 @@ import pygame
 
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
-
 class Unit:
     def __init__(self, pos, type=None):
         self.position = pos
-        
-class Ship:
-    def __init__(self, 
-                pos,
-                name,
-                health, 
-                max_health,
-                shields,
-                max_shields,
-                attack,
-                type=None):
-        self.position = pos
-        self.name = name
-        self.health = health
-        self.max_health = max_health
-        self.shields = shields
-        self.max_shields = max_shields
-        self.attack = attack
-    
-    def info(self):
-        ship_txt = self.name
-        health_txt = "Health: " + str(self.health) + "/" + str(self.max_health)
-        shields_txt = "Shields: " + str(self.shields) + "/" + str(self.max_shields)
-        attack_txt = "Attack: " + str(self.attack)
-        
-        return [ship_txt, health_txt, shields_txt, attack_txt]
-
-
 
 class Map:
     def __init__(self):
@@ -63,12 +34,10 @@ SCREEN_HEIGHT = 640
 # Set up the drawing window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
-ship1 = Ship((2,6),"Ship 1", 50, 100, 75, 75, 20)
-ship2 = Ship((4,2),"Ship 2", 50, 100, 75, 75, 20)
 entities=[]
-entities.append(ship1)    
-entities.append(ship2)
+entities.append(Unit((4,7)))    
+entities.append(Unit((2,6)))
+
 highlight=[]
 selection=None
 
@@ -78,8 +47,6 @@ border_width = 5
 grid_count = 8
 block_size = 73
 block_draw_size = block_size + 2
-selected = False
-
 
 offset = SCREEN_HEIGHT/2 - (grid_count*(block_size)/2)
 
@@ -98,33 +65,37 @@ while running:
             # If the Esc key is pressed, then exit the main loop
             if event.key == K_ESCAPE:
                 running = False
-        if event.type == pygame.QUIT:
-            running = Facurrent_ship = "Current Ship"
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == QUIT:
+            running = False
+        if event.type == MOUSEBUTTONDOWN:
             if(pygame.mouse.get_pressed()[0]):
                 if(mousexx>=0 and mouseyy>=0 and mousexx < grid_count and mouseyy < grid_count):
                     previous = selection
                     selection = (mousexx,mouseyy)
-                    for x in entities:
-                        if selection==x.position:
-                            info = x.info()
-                            print(info)
-                            highlight.clear()
-                            highlight.append(selection)
-                            for nabe in nabes:
-                                xx,yy = (selection[0]+nabe[0],selection[1]+nabe[1])
-                                if(xx>=0 and yy>=0 and xx < grid_count and yy < grid_count):
-                                    highlight.append((xx,yy))
-                            break
+                    print(selection)
+                    if(any([selection==x.position for x in entities])):
+                        highlight.clear()
+                        highlight.append(selection)
+                        for nabe in nabes:
+                            xx,yy = (selection[0]+nabe[0],selection[1]+nabe[1])
+                            if(xx>=0 and yy>=0 and xx < grid_count and yy < grid_count):
+                                highlight.append((xx,yy))
 
 
+    current_ship = "Current Ship"
+
+    health = 100
+    max_health = 100
+    shields = 75
+    max_shields = 75
+    attack = 10
+
+    health_txt = "Health: " + str(health) + "/" + str(max_health)
+    shields_txt = "Shields: " + str(shields) + "/" + str(max_shields)
+    attack_txt = "Attack: " + str(attack)
 
 
-
-    # Fill the background with white
     screen.fill((0, 0, 0))
-
-
 
     sub_width = SCREEN_WIDTH-SCREEN_HEIGHT
     border_width = 5
@@ -135,28 +106,30 @@ while running:
 
     offset = SCREEN_HEIGHT/2 - (grid_count*(block_size)/2)
 
-    
+    image = pygame.image.load("/home/dzarco@cpc.local/Downloads/Space game Images/Blue Spaceship.png")
+    image = pygame.transform.scale(image,(200,200))
+
+
     # Draw menu borders
     pygame.draw.rect(screen, (100, 100, 255), (0, 0, sub_width, SCREEN_HEIGHT), width=border_width)
     pygame.draw.rect(screen, (100, 100, 255), (sub_width, 0, SCREEN_HEIGHT, SCREEN_HEIGHT), width=border_width)
+
+    #UI Text
     font = pygame.font.Font(pygame.font.get_default_font(), 24)
+    ship_display = pygame.font.Font.render(font, current_ship, True, (255, 255, 255))
+    health_display =  pygame.font.Font.render(font, health_txt , True, (255, 255, 255))
+    shields_display =  pygame.font.Font.render(font, shields_txt, True, (255, 255, 255))
+    attack_display =  pygame.font.Font.render(font, attack_txt, True, (255, 255, 255))
 
-
-    i = 0
-    if selection != None:
-        for stat in info:
-            display = pygame.font.Font.render(font, stat, True, (255, 255, 255))
-            if i == 0:
-                screen.blit(display, (20, 20)) 
-            else:
-                screen.blit(display, (20, sub_width + i))
-            i += 50  
-
+    screen.blit(ship_display, (20, 20))
+    screen.blit(image,(55,50))
+    screen.blit(health_display, (20, (sub_width + 50)))
+    screen.blit(shields_display, (20, (sub_width + 100)))
+    screen.blit(attack_display, (20, (sub_width + 150)))   
 
     for y in range(grid_count):
         for x in range(grid_count):
             grid_color = (222, 222, 222)
-
             pygame.draw.rect(
                 screen,
                 grid_color,
@@ -166,6 +139,36 @@ while running:
                 ),
                 width=2
             )
+
+   
+    
+  
+ 
+    
+
+    # Draw menu borders
+    pygame.draw.rect(screen, (100, 100, 255), (0, 0, sub_width, SCREEN_HEIGHT), width=border_width)
+    pygame.draw.rect(screen, (100, 100, 255), (sub_width, 0, SCREEN_HEIGHT, SCREEN_HEIGHT), width=border_width)
+    
+    
+    mousex, mousey = pygame.mouse.get_pos()
+    mousexx = (mousex - sub_width - offset - block_size/2)/block_size
+    mouseyy = (mousey - offset - block_size/2)/block_size
+    if(mousexx > -0.5 and mouseyy > -0.5 and mousexx < grid_count-0.5 and mouseyy < grid_count-0.5):
+        pygame.draw.rect(
+                screen,
+                (200, 200, 0),
+                (
+                    round(mousexx)*block_size + (sub_width + offset), 
+                    round(mouseyy)*block_size + offset,
+                    block_draw_size, block_draw_size
+                ),
+                width=0
+            )
+    BSS = pygame.image.load("/home/dzarco@cpc.local/Downloads/Space game Images/Blue Spaceship.png")
+    width = BSS.get_rect().width
+    height = BSS.get_rect().height
+    BSS = pygame.transform.scale(BSS, (block_size,block_size))
 
     for x,y in highlight:
         grid_color = (100, 100, 255, 100)
@@ -180,15 +183,14 @@ while running:
         )
 
     #Map Entities
-    for entity in entities:
+    for entity in entities:    
         currPos = entity.position
-        pygame.draw.circle(screen, (100,100,255), 
-            (sub_width + currPos[0]*block_size + offset + block_size/2, currPos[1]*block_size + offset + block_size/2),
-            block_size*0.35)
-
-    mousexx = round((mousex - sub_width - offset - block_size/2)/block_size)
-    mouseyy = round((mousey - offset - block_size/2)/block_size)
-
+        screen.blit( BSS, 
+            (sub_width + currPos[0]*block_size + offset, 
+            currPos[1]*block_size + offset)
+        )
+    
+    
     # Draw menu borders
     pygame.draw.line(screen, (100, 100, 255), (0, (sub_width * (4/5))), (sub_width, (sub_width * (4/5))), width=border_width)
     pygame.draw.rect(screen, (100, 100, 255), (0, 0, sub_width, SCREEN_HEIGHT), width=border_width)
@@ -217,5 +219,50 @@ while running:
     clock.tick(60)
 # Done! Time to quit.
 pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
